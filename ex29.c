77a04209269aa8e9c3828afa7effdd21538b3bd0
Dave Yarwood <dave.yarwood@gmail.com>
2016-04-07 11:19:15 -0400

    ex 29 extra credit #1

--- ex29.c
+++ ex29.c
@@ -1,16 +1,18 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include "dbg.h"
 #include <dlfcn.h>
 
-typedef int (*lib_function)(const char* data);
+typedef int (*lib_function)(int data_len, const char* data);
 
 int main(int argc, char* argv[]) {
   int rc = 0;
-  check(argc == 4, "USAGE: ex29 libex29.so function data");
+  check(argc == 5, "USAGE: ex29 libex29.so function data_len data");
 
-  char* lib_file = argv[1];
+  char* lib_file    = argv[1];
   char* func_to_run = argv[2];
-  char* data = argv[3];
+  int data_len      = strtol(argv[3], (char **)NULL, 10);
+  char* data        = argv[4];
 
   void* lib = dlopen(lib_file, RTLD_NOW);
   check(lib != NULL, "Failed to open the library %s: %s", lib_file, dlerror());
@@ -19,8 +21,9 @@ int main(int argc, char* argv[]) {
   check(func != NULL, "Did not find %s function in the library %s: %s",
         func_to_run, lib_file, dlerror());
 
-  rc = func(data);
-  check(rc == 0, "Function %s returned %d for data: %s", func_to_run, rc, data);
+  rc = func(data_len, data);
+  check(rc == 0, "Function %s returned %d for data_len %d, data: %s",
+        func_to_run, rc, data_len, data);
 
   rc = dlclose(lib);
   check(rc == 0, "Failed to close %s", lib_file);
--- libex29.c
+++ libex29.c
@@ -7,28 +7,26 @@ int print_a_message(const char* msg) {
   return 0;
 }
 
-int uppercase(const char* msg) {
-  int i = 0;
+int uppercase(int msg_len, const char* msg) {
+  if (msg_len < 0) return -1;
 
-  // BUG: \0 termination problems
-  for (i = 0; msg[i] != '\0'; i++) {
+  for (int i = 0; i < msg_len; i++) {
     printf("%c", toupper(msg[i]));
   }
   printf("\n");
   return 0;
 }
 
-int lowercase(const char* msg) {
-  int i = 0;
+int lowercase(int msg_len, const char* msg) {
+  if (msg_len < 0) return -1;
 
-  // BUG: \0 termination problems
-  for (i = 0; msg[i] != '\0'; i++) {
+  for (int i = 0; i < msg_len; i++) {
     printf("%c", tolower(msg[i]));
   }
   printf("\n");
   return 0;
 }
 
-int fail_on_purpose(const char* msg) {
+int fail_on_purpose(int msg_len, const char* msg) {
   return 1;
 }
