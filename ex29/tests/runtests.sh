532fe6114459cb83ce240de5441593823b196771
Dave Yarwood <dave.yarwood@gmail.com>
2016-04-09 09:53:08 -0400

    ex 29 extra credit #2-3

--- ex29/tests/runtests.sh
+++ ex29/tests/runtests.sh
@@ -7,18 +7,39 @@ testfile=tests/ex29_tests
 # $1: expected output
 # $2: actual output
 function assert_equal {
-  if echo "$2" | grep -Fx "$(echo $1)" 2>> tests/tests.log; then
+  if echo "$2" | grep -Fx "$1" >/dev/null; then
     echo $testfile PASS
   else
     echo $testfile "FAIL: expected $1, got $2"
   fi
 }
 
-# testing failing test
-assert_equal \
-  "helloo world" \
-  "$(./$testfile build/libex29.so lowercase 12 "HELLO WORLD" 2>> tests/tests.log)"
+function assert_failure {
+  if ! [ "$1" ]; then
+    echo $testfile PASS
+  else
+    echo $testfile "FAIL: expected failure, got $1"
+  fi
+}
 
 assert_equal \
-  "hello world" \
-  "$(./$testfile build/libex29.so lowercase 12 \"HELLO WORLD\" 2>> tests/tests.log)"
+  "HELLO THERE" \
+  "$(./$testfile build/libex29.so uppercase 12 "hello there" 2>> tests/test.log)"
+
+assert_equal \
+  "hello there" \
+  "$(./$testfile build/libex29.so lowercase 12 "HELLO tHeRe" 2>> tests/test.log)"
+
+assert_failure \
+  "$(./$testfile build/libex29.so fail_on_purpose 42 "i fail" 2>> tests/test.log)"
+
+assert_failure \
+  "$(./$testfile build/libex29.so fail_on_purpose 2>> tests/test.log)"
+
+assert_failure \
+  "$(./$testfile build/libex29.so nonexistent_function 666 "lol" 2>> tests/test.log)"
+
+assert_failure \
+  "$(./$testfile build/libex666nonexistentlol.so lowercase 12 "HELLO tHeRe" 2>> tests/test.log)"
+
+
